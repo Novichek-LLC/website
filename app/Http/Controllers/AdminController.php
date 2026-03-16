b57e6febab0983aee9352928ba0ab76dd6292a59
Nova <106208768+XINBOSHIN@users.noreply.github.com>
2026-03-16 04:29:47 +0300

    Delete files

--- app/Http/Controllers/AdminController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Http\Controllers;
-
-class AdminController extends Controller
-{
-    public function layout(){
-        return view('admin.vuexy');
-    }
-}
--- resources/js/components/Loader.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="unload" style="width: 100%; height: 100px;"></div>
-</template>
-
-<script>
-    export default {
-        name: "Loader"
-    }
-</script>
